256f2dd63292da6559df2243cb971a5aaaba2443
Gurinder Bhangu <gurinderbhangu@hotmail.com>
2021-02-18 10:20:41 -0600

    get rid of api route for deleting user

--- server/routes/users.js
+++ server/routes/users.js
@@ -95,16 +95,4 @@ router.post("/login", (req, res, next) => {
     });
 });
 
-router.delete("/:userId", (req, res, next) => {
-  User.remove({ _id: req.params.userId })
-    .exec()
-    .then((result) => res.status(200).json({ message: "User deleted" }))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
 module.exports = router;
