f2df076c27d7eb8462ee0f0524dd980c8978943d
Gurinder Bhangu <gurinderbhangu@hotmail.com>
2021-02-24 14:18:01 -0600

    change participants type to an array of ObjectId

--- server/models/conversation.js
+++ server/models/conversation.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const conversationSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  participants: [{ type: String, ref: "User" }],
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
 module.exports = mongoose.model("Conversation", conversationSchema);
