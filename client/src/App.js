2a8ee52595eb2929a2839a7fce46335dab937216
Gurinder Bhangu <gurinderbhangu@hotmail.com>
2021-03-01 13:12:58 -0600

    combine authorization into current pr

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
 import "./App.css";
 import { theme } from "./themes/theme";
 import Authentication from "./pages/Authentication";
+import ChatWindow from "./pages/ChatWindow";
+import { useState } from "react";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    setCurrentUser(loggedInUser ? JSON.parse(loggedInUser) : null);
+  }, []);
+
   return (
     <div className="app">
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Route path="/">
-            <Redirect to="/register" />
+            <Redirect to={currentUser ? "/chatWindow" : "/register"} />
           </Route>
           <Route
             path="/register"
             render={(props) => (
-              <Authentication {...props} isSignupPage={true} />
+              <Authentication
+                {...props}
+                isSignupPage={true}
+                setCurrentUser={(currentUser) => setCurrentUser(currentUser)}
+              />
             )}
           />
           <Route
             path="/login"
             render={(props) => (
-              <Authentication {...props} isSignupPage={false} />
+              <Authentication
+                {...props}
+                isSignupPage={false}
+                setCurrentUser={(currentUser) => setCurrentUser(currentUser)}
+              />
+            )}
+          />
+          <Route
+            path="/chatWindow"
+            render={(props) => (
+              <ChatWindow {...props} currentUser={currentUser} />
             )}
           />
         </BrowserRouter>
